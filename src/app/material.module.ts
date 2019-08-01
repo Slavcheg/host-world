3a32d6f89a0cd8cc2987ed1cf96871c6295929ae
Georgi Slavchev <Georgi.Slavchev@landmark.co.uk>
2019-08-01 20:01:07 +0300

    Part5: tabs

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -5,14 +5,17 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
+import {MatTabsModule} from '@angular/material/tabs';
 
 
 @NgModule({
     imports: [MatBadgeModule, MatButtonModule, MatToolbarModule,
-        MatIconModule, MatSidenavModule, MatListModule
+        MatIconModule, MatSidenavModule, MatListModule,
+        MatTabsModule
     ],
     exports: [MatBadgeModule, MatButtonModule, MatToolbarModule,
-        MatIconModule, MatSidenavModule, MatListModule
+        MatIconModule, MatSidenavModule, MatListModule,
+        MatTabsModule
     ]
 })
 
